21c20ac9fd475d65dd81400e104fe60fcdf81f71
rodrigochristofoletti <two_rodrigo@hotmail.com>
2024-11-26 13:09:20 +0000

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+
+const routes = new Router();
+
+routes.get('/', (request, response) => {
+    return response.status(200).json({ message: "Hello world!" })
+})
+
+module.exports = routes
