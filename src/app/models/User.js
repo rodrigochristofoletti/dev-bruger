cbd6536610b5914ff49d21803cbd355ed379de4b
rodrigochristofoletti <two_rodrigo@hotmail.com>
2024-12-03 12:52:11 +0000

    SessionControlle.js created. Updates on User.js. Bcrypto framework installed.

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,22 +1,34 @@
-//import Sequelize, { Model } from "sequelize";
-const Sequelize = require('sequelize');
+import Sequelize, { Model } from "sequelize";
+
 const { Model } = Sequelize;
 
+import bcrypt from "bcrypt";
 
 class User extends Model {
   static init(sequelize) {
-    super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password_hash: Sequelize.STRING,
-      admin: Sequelize.BOOLEAN,
-    },
-    {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        admin: Sequelize.BOOLEAN,
+      },
+      {
         sequelize,
-    }
-);
+      }
+    );
+
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10);
+      }
+    });
+    return this;
+  }
+
+  async checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
-
-module.exports = User;
-//export default User;
+export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+
 
 const routes = new Router();
 
-routes.get("/", (request, response) => {
-  return response.status(200).json({ message: "Hello world!" });
-});
+routes.post("/users", UserController.store);
+routes.post("/session", SessionController.store);
 
 export default routes;
